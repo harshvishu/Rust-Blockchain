ee8608d48ababec0c03b06538a8b3f260fd39141
Harsh Vishwakarma <mhacnagbani@gmail.com>
2021-02-18 10:55:51 +0530

    cleaned main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,31 +11,5 @@ mod sha;
 
 fn main() {
     println!("Hello, Blockchain!");
-    let mut chain = Chain::new();
-
-    chain.new_transaction("0".to_string(), Chain::node_identifier(), 1);
-    chain.new_transaction("Harsh".to_string(), "Batman".to_string(), 10_000_00);
-
-    match chain.last_block() {
-        Some(last_block) => {
-            let previous_hash = Chain::hash(last_block);
-            chain.new_block(Some(previous_hash), 200);
-        }
-        None => {}
-    };
-
-    chain.new_transaction("Harsh".to_string(), "Superman".to_string(), 20);
-    chain.new_transaction("Harsh".to_string(), "Spider man".to_string(), 70);
-    chain.new_transaction("Batman".to_string(), "Spider man".to_string(), 20000);
-
-    match chain.last_block() {
-        Some(last_block) => {
-            let previous_hash = Chain::hash(last_block);
-            chain.new_block(Some(previous_hash), 200);
-        }
-        None => {}
-    };
-
-    chain.new_transaction("Popoye".to_string(), "Brutus".to_string(), 500);
     server::rocket().launch();
 }
